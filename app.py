8b2edbeaf6231ae3f7ad1337adea16eb352b9352
mhajimorad <aminh@eecs.berkeley.edu>
2021-07-17 15:21:09 +0000

    my first giphy upload

--- app.py
+++ app.py
@@ -2,15 +2,31 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
 app = Flask(__name__)
+app.config["GIPHY_KEY"] = os.getenv("GIPHY_KEY")
+key = app.config["GIPHY_KEY"]
 
+# is_prod = os.environ.get('IS_HEROKU', None)
+# if is_prod:
+#     #print("HEROKU!!!")
+#     heroku config:set key="..."
+# else:
+#     app.config["GIPHY_KEY"] = os.getenv("GIPHY_KEY")
+#     key = app.config["GIPHY_KEY"]
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+@app.route('/yourgif', methods=['GET','POST'])
+def yourgif():
+    query = request.form["gifchoice"]
+    
+    imgURL = getImageUrlFrom(query, key)
+    return render_template("yourgif.html", time = datetime.now(), imgURL=imgURL)
--- model.py
+++ model.py
@@ -1,2 +1,7 @@
-def getImageUrlFrom(query):
-    pass
+import requests
+
+def getImageUrlFrom(query, key):
+    endPointURL = f"https://api.giphy.com/v1/gifs/search?api_key={key}&q={query}&limit=25&offset=0&rating=g&lang=en"
+    response = requests.get(endPointURL).json()
+    imgURL = response["data"][0]["images"]["fixed_height"]["url"]
+    return imgURL
